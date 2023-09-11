5ce727f1d2da8fcdb35fe1a637b90b5c8642bc7b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-11 10:14:07 -0300

    a

---
+++ mongoDB/cadaescola.js
@@ -0,0 +1,42 @@
+// mongodb://localhost:27017
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/escola',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 20000
+})
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function(){
+    console.log("Estamos conectados ao mongodb")
+})
+
+
+const alunoSchema = new mongoose.Schema({
+    matricula : String,
+    nome : String, 
+    idade : Number,
+    turma : String
+});
+
+const Aluno = mongoose.model("Aluno", alunoSchema)
+
+const paulo = new Aluno({
+    matricula : 'rm201',
+    nome : "Paulo da Silva",
+    idade : 18,
+    turma : '2MIA'
+})
+
+paulo.save();
+
+Aluno.findOne({nome : "Paulo da Silva"}, function(err,Aluno){
+    console.log(Aluno)
+})
+//arrumar
